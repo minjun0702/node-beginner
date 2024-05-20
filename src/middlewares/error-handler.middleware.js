c93449cb17773f1f0c00851cf95e8c5878cd6e27
leeminjun <mlnsuk0702@gmail.com>
2024-05-20 15:57:39 +0900

    feat:에러처리 미들웨어 구현

---
+++ src/middlewares/error-handler.middleware.js
@@ -0,0 +1,13 @@
+export const errorHandler = (err, req, res, next) => {
+  console.log(err);
+
+  return res
+    .status(500)
+    .json({ status: 500, message: '예상치 못한 에러가 발생했습니다.' });
+};
+
+//에러 핸들러로 발생시키려면, router 모든 요청 건에 next를 넣어야 하며,
+
+// try, catch로 감싸줘야함
+// try {
+//} catch(error) {next(error)};
